ec77a04b7e51b656f9ea924eb9676926a428098e
Daniel Tamayo <elkin.zapata@est.iudigital.edu.co>
2024-09-17 15:50:06 -0500

    sUBIENDO MI PRIMER REPOSITORIO

--- src/Books.java
+++ src/Books.java
@@ -1,2 +1,25 @@
 public class Books {
+    // Atributos
+    private String title;
+    private String author;
+    private String isbn;
+
+
+
+    // Constructor
+    public Books(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    // Métodos
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
